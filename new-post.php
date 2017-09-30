45c00127b59cfc5a491e798b5dedb695588becc2
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-09-30 12:19:24 -0400

    posts do the thing now

--- new-post.php
+++ new-post.php
@@ -1,12 +1,19 @@
 Thanks for submitting!<br>
 Good location! -> <?php echo $_POST['loc']; ?>.<br>
 DESC: <?php echo $_POST['desc']; ?><br>
-<?php $d = date("h:i:sa") ?>
+<?php
+  date_default_timezone_set('America/New_York');
+  $d = time();
+  $ddisp = date("n/j/y h:i:sa");
+?>
 Time: <?php echo $d ?>.<br>
 <?php
   mkdir("posts");
   $fname = "posts/" . $d . ".post";
-  $fcontents = $_POST['loc'] . "\n" . $_POST['desc'];
+  $fcontents =
+    $ddisp . "\n" .
+    $_POST['loc'] . "\n" .
+    substr($_POST['desc'], 0, 100);
   file_put_contents($fname,
     $fcontents);
 ?>
--- submit-new.php
+++ submit-new.php
@@ -3,6 +3,6 @@
 <body>
 <form action="new-post.php" method="post">
   <p>Location: <input type="text" name="loc"/></p>
-  <p>Description: <textarea name="desc"></textarea></p>
+  <p>Description: <textarea name="desc" maxlength="100"></textarea></p>
   <p><input type="submit" /></p>
 </form>
