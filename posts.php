747bc78209c1e8170ef0324f1d56b3855ddf1305
Bryce Thuilot <bthuilot@gmail.com>
2017-09-30 13:46:05 -0400

    Css for posts

--- posts.php
+++ posts.php
@@ -10,23 +10,25 @@ foreach ($files as &$f) {
     continue;
 ?>
 <div class="row post">
-  <div class="location-time">
-    <div class="location-formatting">
-      <p class="location">
-        <?php echo $loc; ?>
+  <div class='location-time-map'>
+    <div class="location-time">
+      <div class="location-formatting">
+        <p class="location">
+          <?php echo $loc; ?>
+        </p>
+      </div>
+      <p class="time">
+        <?php echo $date; ?>
       </p>
     </div>
-    <p class="time">
-      <?php echo $date; ?>
-    </p>
-  </div>
-  <div class="map">
-    <iframe
-       width="100"
-       height="100"
-       frameborder="0" style="border:0"
-       src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCQjxdV0gtbzW2a2sF253fckRP4fUUx1u0&q=<?php echo str_replace(" ", "+", $loc); ?>+in+Boston,+MA" allowfullscreen>
-    </iframe>
+    <div class="map">
+      <iframe
+         width="300"
+         height="300"
+         frameborder="0" style="border:0"
+         src="https://www.google.com/maps/embed/v1/place?key=AIzaSyCQjxdV0gtbzW2a2sF253fckRP4fUUx1u0&q=<?php echo str_replace(" ", "+", $loc); ?>+in+Boston,+MA" allowfullscreen>
+      </iframe>
+    </div>
   </div>
   <div class="desc">
     <p class="desc-text">
