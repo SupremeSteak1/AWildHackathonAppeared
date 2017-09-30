49160609fa3f6c31eaa6bc43fa56c9fbc27c9020
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-09-30 11:57:51 -0400

    posts

--- new-post.php
+++ new-post.php
@@ -1,4 +1,7 @@
 Thanks for submitting!<br>
 Good location! -> <?php echo $_POST['loc']; ?>.<br>
 DESC: <?php echo $_POST['desc']; ?><br>
-Time: <?php echo date("h:i:sa") ?>.
+<?php $d = date("h:i:sa") ?>
+Time: <?php echo $d ?>.<br>
+$handle = fopen($d + ".post", "w+");
+fwrite($handle
