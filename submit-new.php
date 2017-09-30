77fe70ec50a7ec9645561e311a323a21c3c66195
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-09-30 13:13:38 -0400

    index form works

--- submit-new.php
+++
@@ -1,8 +0,0 @@
-<DOCTYPE html>
-<html>
-<body>
-<form action="new-post.php" method="post">
-  <p>Location: <input type="text" name="loc"/></p>
-  <p>Description: <textarea name="desc" maxlength="100"></textarea></p>
-  <p><input type="submit" /></p>
-</form>
