4af77d44df013e70d55c4ca33aef0e210011c0bf
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-10-01 17:09:50 -0400

    sanitize

--- report.php
+++ report.php
@@ -1,6 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-rename("posts/" . $_POST["var"], "reported/" . $_POST["var"]);
+  if (1 == preg_match("^\d{10}.post$",$_POST["var"])) {
+    rename("posts/" . $_POST["var"] . ".post", "reported/" . $_POST["var"] . ".post");
+  }
 }
 echo "<script>window.location.replace('/');</script>";
 ?>
