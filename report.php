e7a7393010071125429df36f2eec9ad862467872
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-10-01 17:18:17 -0400

    fixed

--- report.php
+++ report.php
@@ -1,7 +1,8 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-  if (1 == preg_match("^\d{10}.post$",$_POST["var"])) {
-    rename("posts/" . $_POST["var"] . ".post", "reported/" . $_POST["var"] . ".post");
+  if (1 == preg_match("/^\d{10}\.post$/",$_POST["var"])) {
+    mkdir("reported", 0755, true);
+    rename("posts/" . $_POST["var"], "reported/" . $_POST["var"]);
   }
 }
 echo "<script>window.location.replace('/');</script>";
