73bb521c069f8942582f9301c36b9cf7a1fb311d
Kyle Sferrazza <kyle.sferrazza@gmail.com>
2017-10-06 12:26:37 -0400

    wording

--- query/index.php
+++ query/index.php
@@ -6,7 +6,7 @@ $date = filter_var($array[0], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $loc = filter_var($array[1], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 $desc = filter_var($array[2], FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-$spch = "Aoun is at " . $loc;
+$spch = "Aoun was last seen at " . $loc . ".";
 $response->speech = $spch;
 $response->display = $spch;
 $response->source = "WheresAoun.tk";
